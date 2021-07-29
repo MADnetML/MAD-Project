d3d01effd944f1226e0c371fad019cb9737d7919
PeAriel <82453690+PeAriel@users.noreply.github.com>
2021-07-29 13:25:33 +0300

    generating folder for training and validation if it does not exists

--- generate_data.py
+++ generate_data.py
@@ -6,6 +6,14 @@ import os
 
 
 def save_data(number_of_samples, measurement_size, kernel_size, SNR=2, training=False, validation=False):
+
+    files_in_folder = os.listdir()
+    if training and 'training_dataset' not in files_in_folder:
+        os.system("mkdir training_dataset")
+    if validation and 'validation_dataset' not in files_in_folder:
+        os.system("mkdir validation_dataset")
+
+
     defect_density = np.random.uniform(low=-4, high=-1, size=(number_of_samples,))
 
     E, n1, n2 = measurement_size
@@ -16,15 +24,15 @@ def save_data(number_of_samples, measurement_size, kernel_size, SNR=2, training=
                                                                        10 ** defect_density[i],
                                                                        SNR)
         if training:
-            torch.save(torch.tensor(fix_levels(temp_kernel, E)), os.getcwd() + '/training/kernel_%d.pt' % i)
-            torch.save(torch.tensor(fix_levels(temp_measurement, E)), os.getcwd() + '/training/measurement_%d.pt' % i)
-            torch.save(torch.tensor(temp_activation_map), os.getcwd() + '/training/activation_%d.pt' % i)
+            torch.save(torch.tensor(fix_levels(temp_kernel, E)), os.getcwd() + '/training_dataset/kernel_%d.pt' % i)
+            torch.save(torch.tensor(fix_levels(temp_measurement, E)), os.getcwd() + '/training_dataset/measurement_%d.pt' % i)
+            torch.save(torch.tensor(temp_activation_map), os.getcwd() + '/training_dataset/activation_%d.pt' % i)
         elif validation:
-            torch.save(torch.tensor(fix_levels(temp_kernel, E)), os.getcwd() + '/validation/kernel_%d.pt' % i)
-            torch.save(torch.tensor(fix_levels(temp_measurement, E)), os.getcwd() + '/validation/measurement_%d.pt' % i)
-            torch.save(torch.tensor(temp_activation_map), os.getcwd() + '/validation/activation_%d.pt' % i)
-        else:
-            print("Specify validation or training to save files.")
+            torch.save(torch.tensor(fix_levels(temp_kernel, E)), os.getcwd() + '/validation_dataset/kernel_%d.pt' % i)
+            torch.save(torch.tensor(fix_levels(temp_measurement, E)), os.getcwd() + '/validation_dataset/measurement_%d.pt' % i)
+            torch.save(torch.tensor(temp_activation_map), os.getcwd() + '/validation_dataset/activation_%d.pt' % i)
+    if not training and not validation:
+        print("Specify validation or training to save files.")
 
 
 
@@ -32,4 +40,5 @@ def save_data(number_of_samples, measurement_size, kernel_size, SNR=2, training=
 number_of_samples = 2
 measurement_size = (100, 200, 200)  # = (E, n1, n2)
 kernel_size = (20, 20)  # = (m1, m2)
-save_data(number_of_samples, measurement_size, kernel_size)
+save_data(number_of_samples, measurement_size, kernel_size, training=True)
+save_data(number_of_samples, measurement_size, kernel_size, validation=True)
